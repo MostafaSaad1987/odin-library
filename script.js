d55bbdd4b421b085a5b950d75a38fb715dc12b62
MostafaSaad1987 <52860974+MostafaSaad1987@users.noreply.github.com>
2023-06-16 15:56:57 +0300

    Library function complete. Image addition functionality. More changes.

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 class Book {
-    constructor(title, author, noOfPages, read) {
+    constructor(title, author, noOfPages, read, image) {
         this.title = title;
         this.author = author;
         this.noOfPages = noOfPages;
         this.read = read;
+        this.image = image;
     }
 }
 
@@ -17,6 +18,11 @@ Book.prototype.info = function () {
     return `${this.title} by ${this.author}, ${this.noOfPages} pages, ${tempRead}`;
 }
 
+
+Book.prototype.toggleRead = function () {
+    this.read = !this.read;
+}
+
 let library = [];
 
 const addButton = document.querySelector("#add-button");
@@ -25,42 +31,123 @@ const addAuthor = document.querySelector("#author");
 const addPages = document.querySelector("#pages");
 const hasBeenRead = document.querySelector("#read");
 const bookForm = document.querySelector("#my-form");
+const libraryArea = document.querySelector(".libraryArea")
+
+const imgSource = document.querySelector("#bookCover");
 
 function addBookToLibrary() {
-
-    let newBook = new Book(addTitle.value, addAuthor.value, addPages.value, hasBeenRead.checked);
+    let newBook = new Book(addTitle.value, addAuthor.value, addPages.value, hasBeenRead.checked, imgSource.value);
     library.push(newBook);
 
+    removeAllChildNodes(libraryArea);
     printBooks();
 
     bookForm.reset();
 }
 
 function printBooks() {
-    console.clear();
-
     library.forEach(element => {
-        console.log(element.info());
+        createCard(element);
     });
 }
 
-function createCard() {
-    
+function createCard(enteredBook) {
+    let newBook = document.createElement('div');
+    newBook.classList = "card";
+
+    let titleText = document.createElement('p');
+    titleText.classList = "small-text";
+    titleText.textContent = "Title";
+
+    let titleContainer = document.createElement('div');
+    let newTitle = document.createElement('p');
+    newTitle.textContent = enteredBook.title;
+
+    titleContainer.appendChild(titleText);
+    titleContainer.appendChild(newTitle);
+
+    newBook.appendChild(titleContainer);
+
+
+
+    let authorText = document.createElement('p');
+    authorText.classList = "small-text";
+    authorText.textContent = "Author";
+
+    let authorContainer = document.createElement('div');
+    let newAuthor = document.createElement('p');
+    newAuthor.textContent = enteredBook.author;
+
+    authorContainer.appendChild(authorText);
+    authorContainer.appendChild(newAuthor);
+
+    newBook.appendChild(authorContainer);
+
+
+
+    let coverImage = document.createElement('img');
+    coverImage.classList = "cover";
+
+    if (enteredBook.image == null || enteredBook.image == "" || !isImage(enteredBook.image)) {
+        coverImage.src = "placeholder.png";
+        coverImage.style.opacity = "0";
+    } else {
+        coverImage.src = enteredBook.image;
+    }
+
+    newBook.appendChild(coverImage);
+
+
+
+
+    let pagesText = document.createElement('p');
+    pagesText.classList = "small-text";
+    pagesText.textContent = "Pages";
+
+    let pagesContainer = document.createElement('div');
+    let newPages = document.createElement('p');
+    newPages.textContent = enteredBook.noOfPages;
+
+    pagesContainer.appendChild(pagesText);
+    pagesContainer.appendChild(newPages);
+
+    newBook.appendChild(pagesContainer);
+
+
+
+    let readText = document.createElement('p');
+    readText.textContent = "Read";
+    readText.style.color = "white";
+
+    let readContainer = document.createElement('div');
+    if (enteredBook.read) {
+        readContainer.style.backgroundColor = "green";
+    } else {
+        readContainer.style.backgroundColor = "red";
+    }
+
+    readContainer.appendChild(readText);
+    newBook.appendChild(readContainer);
+
+
+
+    libraryArea.appendChild(newBook);
+}
+
+function isImage(url) {
+    return /\.(jpg|jpeg|png|webp|avif|gif|svg)$/.test(url);
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
 }
 
 /*
-Write a function that loops through the array and displays each book on the page.
-You can display them in some sort of table, or each on their own “card”.
-It might help for now to manually add a few books to your array so you can see the display.
-
-
 Add a “NEW BOOK” button that brings up a form allowing users to input the details for
 the new book: author, title, number of pages, whether it’s been read and anything else
-you might want. You will most likely encounter an issue where submitting your
-form will not do what you expect it to do. That’s because the submit input tries
-to send the data to a server by default. If you’ve done the bonus section for the
-calculator assignment, you might be familiar with event.preventDefault();.
-Read up on the event.preventDefault documentation again and see how you can solve this issue!
+you might want.
 
 Add a button on each book’s display to remove the book from the library.
 
@@ -68,9 +155,4 @@ You will need to associate your DOM elements with the actual book objects in som
 One easy solution is giving them a data-attribute that corresponds to the index of the library array.
 
 Add a button on each book’s display to change its read status.
-
-To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance.
-
-NOTE: You’re not required to add any type of storage right now. You will have the option to come back to this project later on in the course.
-
 */
